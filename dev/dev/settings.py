d1e308bff0177b53c0968151c904eefdb41029eb
69git69 <83546495+69git69@users.noreply.github.com>
2022-10-15 17:23:34 +0530

    Update settings.py

--- dev/dev/settings.py
+++ dev/dev/settings.py
@@ -22,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-=#r$lm6(u!@y&k=8o89)n36oow!t0lsud5)ut_^3hha(&k!a$u'
-SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
